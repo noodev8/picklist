5afd7ef64dd2e0733986b1f9b2eb44262ce4cd09
Summer2906 <Summer.louise2906@gmail.com>
2025-06-09 11:43:34 +0100

    version 1.0.2

--- picklist_flutter/lib/config/app_config.dart
+++ picklist_flutter/lib/config/app_config.dart
@@ -10,13 +10,13 @@ Contains API endpoints, server URLs, and other app-wide configuration settings
 class AppConfig {
   // API Configuration
   //satic const String apiBaseUrl = 'http://192.168.1.182:3000'; // summer office
-  static const String apiBaseUrl = 'http://192.168.1.173:3000'; // summer shrewsbury
+  //static const String apiBaseUrl = 'http://192.168.1.173:3000'; // summer shrewsbury
   // static const String apiBaseUrl = 'http://77.68.13.150:3007'; // Test Server
-  //static const String apiBaseUrl = 'https://picklist.noodev8.com'; // Prod Server
+  static const String apiBaseUrl = 'https://picklist.noodev8.com'; // Prod Server
 
   // App Settings
   static const String appName = 'Pick List';
-  static const String appVersion = '1.0.1';
+  static const String appVersion = '1.0.2';
  
   // API Endpoints
   static const String getPicksEndpoint = '/get_picks';
